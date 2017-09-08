189f0df79ea7357226ae8d1a468340e1ffd48528
Luke Bogus <lbogus@luke-boguss-mbp.unl.edu>
2017-09-07 21:21:09 -0500

    Line 10: added 'and Luke Bogus'

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,12 +1,12 @@
 /**
   Author: Keenan Allen
   Date: 9/7/2017
-  Hello World in C 
-*/ 
+  Hello World in C
+*/
 
-#include <stdio.h> 
- 
+#include <stdio.h>
+
 int main(int argc, char **argv) {
-  printf("Keenan Allen\n");
+  printf("Keenan Allen and Luke Bogus\n");
   return 0;
 }
